b2f20bf90ec7abc4b2063b85dbe968e37ff144ee
Gabrielle Alcantara <gabrielle_alcantara@jabil.com>
2021-11-26 17:34:35 -0300

    feat: :sparkles: add product entity configuration

---
+++ Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Description)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Price)
+                .HasPrecision(10, 2);
+
+            builder.HasOne(x => x.Category)
+                .WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
